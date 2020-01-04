8661f51b9273c462e731fcc24212fbb5833810d5
r-ba <rhysbalevicius@gmail.com>
2020-01-04 03:43:51 -0500

    Refactor Header status display

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -4,9 +4,17 @@ import closeIcon from './../assets/close.png';
 import '../styles/Header.css';
 
 function Header(props) {
-  let iconColour = 'grey';
-  if (props.data.agentStatus === 'Online') iconColour = 'green';
-  else if (props.data.agentStatus === 'Away') iconColour = 'orange';
+
+  let status;
+  const agentStatus = props.data.agentStatus;
+
+  if (agentStatus) {
+    let iconColour = 'grey';
+    if (agentStatus === 'Online') iconColour = 'green';
+    else if (agentStatus === 'Away') iconColour = 'orange';
+    const statusIcon = <span className={`status-icon ${iconColour}`}></span>;
+    status = <div className="status">{statusIcon}{agentStatus}</div>;
+  }
 
   return (
     <div className="header">
@@ -19,12 +27,7 @@ function Header(props) {
         <div>
           {props.data.name}
         </div>
-        <div className="status">
-          <span class={`status-icon ${iconColour}`}></span>
-          {
-            props.data.agentStatus
-          }
-        </div>
+        { status }
       </div>
       <div className="header-close-button" onClick={props.onToggle}>
         <img src={closeIcon} alt="Close Chat" />
