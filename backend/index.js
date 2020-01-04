60dbb3e4290e6409fe304b1c42bde58c66b8c595
r-ba <rhysbalevicius@gmail.com>
2020-01-03 23:10:03 -0500

    Add default agent profile

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,21 @@
 const http = require('http');
-const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const server = http.createServer(app);
 
+const agentProfile = require('./agent-profile');
+const startAgent = require('./agent');
+startAgent(process.env.SOCKET_PORT || 8080);
+
+app.use(cors());
 app.set('port', process.env.PORT || 4000);
 const port = app.get('port');
 
 app.get('/', (req, res) => {
-  res.json({});
+  res.json(agentProfile);
 });
-
-const io = require('socket.io')();
-
-const agentData = {
-  icon: 'images/user.png',
-  name: 'Username',
-  status: 'Offline',
-  unseenMessages: 0
-};
-
-io.on('connection', client => {
-  client.emit('agent-info', agentData);
-  client.on('subscribeToChat', (interval) => {
-    // ...
-  });
-});
-io.listen(8080);
 
 server.listen(port, function(){
-  console.log(`Server started on port ${port}...`)
+  console.log(`Server started on port ${port}...`);
 });
